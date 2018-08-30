1d418b7e3ef654c261fbdfd823ed713c67498b2f
iwong <iwong30@gatech.edu>
2018-08-29 21:01:19 -0400

    nothing much

--- mytest.py
+++ mytest.py
@@ -9,30 +9,37 @@ if platform.system() != 'Windows':
     import resource
 from time import time, sleep
 
-print ""
 try:
-    sample_board = Board(RandomPlayer(), RandomPlayer())
-    # setting up the board as though we've been playing
-    sample_board.move_count = 2
-    sample_board.__board_state__ = [
-        ["Q1", " ", " ", " ", " ", " ", " "],
-        [ " ", " ", " ", " ", " ", " ", " "],
-        [ " ", " ", " ", " ", " ", " ", " "],
-        [ " ", " ", " ","Q2", " ", " ", " "],
-        [ " ", " ", " ", " ", " ", " ", " "],
-        [ " ", " ", " ", " ", " ", " ", " "],
-        [ " ", " ", " ", " ", " ", " ", " "]
+    """Example test to make sure
+    your minimax works, using the
+    OpenMoveEvalFunction evaluation function.
+	This can be used for debugging your code
+	with different model Board states. 
+	Especially important to check alphabeta 
+	pruning"""
+    # create dummy 5x5 board
+    b = Board(RandomPlayer(), HumanPlayer(), 5, 5)
+
+    b.__board_state__ = [
+        [" ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " "],
+        [" ", " ", " ", "Q1", " "],
+        [" ", " ", " ", "Q2", " "],
+        [" ", " ", " ", " ", " "]
     ]
-    sample_board.__last_queen_move__ = {sample_board.__queen_1__: (0, 0, False), \
-                                        sample_board.__queen_2__: (3, 3, False)}
-    test = sample_board.get_legal_moves()
-    print(len(test))
-    opp_moves = sample_board.get_opponent_moves()
-    print(len(opp_moves))
-    h = OpenMoveEvalFn()
-    print 'OpenMoveEvalFn Test: This board has a score of %s.' % (h.score(sample_board))
+    b.__last_queen_move__[b.__queen_1__] = (2, 3, False)
+    b.__last_queen_move__[b.__queen_2__] = (3, 3, False)
+    b.move_count = 2
+
+    output_b = b.copy()
+    legal_moves = b.get_legal_moves()
+    winner, move_history, termination = b.play_isolation(
+        time_limit=100000, print_moves=True)
+    print 'Minimax Test: Runs Successfully'
+    # Uncomment to see example game
+    # print game_as_text(winner, move_history, termination, output_b)
 except NotImplementedError:
-    print 'OpenMoveEvalFn Test: Not implemented'
+    print 'Minimax Test: Not Implemented'
 except:
-    print 'OpenMoveEvalFn Test: ERROR OCCURRED'
+    print 'Minimax Test: ERROR OCCURRED'
     print traceback.format_exc()
